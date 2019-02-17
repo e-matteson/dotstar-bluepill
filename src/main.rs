db97735ab7ecf5d78f9d5293fc7a5b4706838387
e-matteson <e.r.matteson@gmail.com>
2019-02-17 12:18:36 -0500

    feat: switch to new HAL crate, add encoder demo

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 #![no_main]
 
 extern crate panic_semihosting;
-extern crate stm32f103xx_hal as hal;
+extern crate stm32f1xx_hal as hal;
 
 use cortex_m_rt::entry;
 use cortex_m_semihosting::hprintln;
@@ -11,10 +11,11 @@ use hal::{
     delay::Delay,
     prelude::*,
     spi::{Mode, Phase, Polarity, Spi},
-    stm32f103xx,
+    qei::Qei,
+    stm32
 };
 
-use dotstar::{ColorRgb, Demo, DemoSettings, DotstarStrip, Duration, LightShow};
+use dotstar::{ColorRgb, CircleShow, CircleShowSettings, DotstarStrip, Duration, LightShow, FlashyShow};
 
 #[entry]
 fn main() -> ! {
@@ -22,7 +23,7 @@ fn main() -> ! {
 
     // Get access to peripherals
     let cp = cortex_m::Peripherals::take().unwrap();
-    let dp = stm32f103xx::Peripherals::take().unwrap();
+    let dp = stm32::Peripherals::take().unwrap();
     let mut rcc = dp.RCC.constrain();
     let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
 
@@ -42,6 +43,13 @@ fn main() -> ! {
     let miso = gpioa.pa6;
     let mosi = gpioa.pa7.into_alternate_push_pull(&mut gpioa.crl);
 
+    // Get quadrature encoder pins
+    let c1 = gpioa.pa0;
+    let c2 = gpioa.pa1;
+    let encoder = Qei::tim2(dp.TIM2, (c1, c2), &mut afio.mapr, &mut rcc.apb1);
+
+    let button = gpioa.pa2.into_pull_up_input(&mut gpioa.crl);
+
     let spi = Spi::spi1(
         dp.SPI1,
         (sck, miso, mosi),
@@ -55,17 +63,26 @@ fn main() -> ! {
         &mut rcc.apb2,
     );
 
-    let mut settings = DemoSettings::default();
-    settings.brightness = 30;
-    let mut demo = Demo::new(&settings);
+    // let mut settings = CircleShowSettings::default();
+    // settings.brightness = 20;
+    // let mut demo = CircleShow::new(&settings);
+    let mut demo = FlashyShow::new(&());
     let mut strip = DotstarStrip::new(spi);
     let mut lights = [ColorRgb { r: 0, g: 0, b: 0 }; 100];
 
     let period = 10u32;
     let mut duration = Duration::Millis(0);
     loop {
+        delay.delay_ms(500_u16);
+        hprintln!("{}", encoder.count()).unwrap();
+        if button.is_low() {
+            hprintln!("button down!").unwrap();
+        }
+
         if duration.is_zero() {
+            // hprintln!("before").unwrap();
             duration = demo.next(&mut lights);
+            // hprintln!("after").unwrap();
             strip.send(&lights).expect("failed to send");
         }
         delay.delay_ms(period);
